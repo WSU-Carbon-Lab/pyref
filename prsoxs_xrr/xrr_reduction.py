96231167cfae09b4611701813e5ae4b8a7cc773c
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2023-04-17 14:53:18 -0700

    update

--- prsoxs_xrr/xrr_reduction.py
+++ prsoxs_xrr/xrr_reduction.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import typing
 import abc
+from sympy import is_monotonic
+from sympy import is_monotonic
 from uncertainties import unumpy, ufloat
 import matplotlib.pyplot as plt
 
@@ -93,17 +95,7 @@ def locate_spot(image: np.ndarray, edge_cut: int) -> tuple:
 
 def normalization(refl: pd.DataFrame):
     """
-    _summary_
-
-    Parameters
-    ----------
-    refl : pd.DataFrame
-        _description_
-
-    Returns
-    -------
-    _type_
-        _description_
+    Normalization
     """
     # find the cutoff for i_zero points
     izero_count = refl["Q"].where(refl["Q"] == 0).count()
@@ -114,4 +106,79 @@ def normalization(refl: pd.DataFrame):
         izero = uaverage(refl["R"].iloc[:izero_count])
 
     refl["R"] = refl["R"] / izero  # type: ignore
+    refl["R"] = refl["R"].drop(refl["R"].index[:izero_count])
     return refl
+
+
+def stitching(refl: pd.DataFrame):
+    """
+    stitch reflectivity curve
+    """
+
+    # unpack refl
+    Q = refl["Q"].to_numpy()
+    R = refl["R"].to_numpy()
+
+    def split_increasing(arr, image):
+        """
+        Splits the given array `arr` into monotonically decreasing subarrays
+        of size at least 2 and first entries being at least `thre`.
+        """
+        split_points = np.where(np.diff(arr) < 0)[0] + 1
+
+        sub_lists_dom = np.split(arr, split_points)
+        sub_lists_im = np.split(image, split_points)
+
+        result_dom = [sub for sub in sub_lists_dom if sub.size > 1]
+        result_im = [sub for sub in sub_lists_im if sub.size > 1]
+
+        return result_dom, result_im
+
+    subsets = split_increasing(Q, R)
+
+    return subsets
+
+
+if __name__ == "__main__":
+    refl = pd.DataFrame(columns=["Q", "R"])
+    refl["Q"] = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        1,
+        1,
+        2,
+        3,
+        4,
+        5,
+        2,
+        2,
+        3,
+        4,
+        5,
+        6,
+    ]
+    refl["R"] = [
+        10,
+        11,
+        12,
+        13,
+        14,
+        11,
+        11,
+        12,
+        13,
+        14,
+        15,
+        12,
+        12,
+        13,
+        14,
+        15,
+        16,
+    ]
+    stitches = stitching(refl)
+
+    print(stitches)

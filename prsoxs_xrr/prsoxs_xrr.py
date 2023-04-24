9f3c0d4192fed1452a17606a37b0760922d8bbf6
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2023-04-24 01:22:29 -0700

    Stitching works!

--- prsoxs_xrr/prsoxs_xrr.py
+++ prsoxs_xrr/prsoxs_xrr.py
@@ -1,60 +1,74 @@
 """Main module."""
 
-from cProfile import label
 import os
 import numpy as np
 import pandas as pd
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from uncertainties import unumpy
+import glob
 
 from xrr_toolkit import scattering_vector, uaverage
 
 
 class XRR:
-    """Class for X-ray reflectometry data analysis."""
+    """
+    Main class for processing xrr data
+    """
 
-    def __init__(self, directory: str):
+    def __init__(self, directory: str, error_method=None, *args, **kwargs):
         self.directory = directory
-        self.q = []
-        self.r = []
+        self.q = np.array([], dtype=np.float64)
+        self.r = np.array([], dtype=np.float64)
         self.xrr = pd.DataFrame()
         self.std_err = None
         self.shot_err = None
-        self.currents = None
 
-    def load(self, error_method="shot", stitch_color="yes") -> None:
+    # define the outcome of the call
+
+    def calc_xrr(self, *args, **kwargs):
+        # Load fits and perform data reduction, stitching, and normalization
+        self._fits_loader()
+        self._data_reduction(*args, **kwargs)
+        self._normalize()
+        self._stitch(*args, **kwargs)
+
+        # Assign values to xrr dictionary
+        self.xrr["Q"] = self.q
+        self.xrr["R"] = unumpy.nominal_values(self.r)
+        self.xrr["R_err"] = unumpy.std_devs(
+            self.r
+        )  # call std_devs() to calculate standard deviation of self.r
+
+        # Return xrr dictionary
+        return self.xrr
+
+    def _fits_loader(self) -> None:
         """Load X-ray reflectometry data from FITS files in the specified directory."""
 
+        # Get list of .fits files in the directory
+        file_list = sorted(glob.glob(os.path.join(self.directory, "*.fits")))
+
         # Pre-allocate memory for the numpy arrays
-        file_list = [f for f in os.scandir(self.directory) if f.name.endswith(".fits")]
-        num_files = len(file_list)
-        self.energies = np.empty(num_files)
-        self.sample_theta = np.empty(num_files)
-        self.beam_current = np.empty(num_files)
-        self.images = np.empty((num_files, 500, 500))
+        arrays = [
+            [
+                fits.getheader(f, 0)["Beamline Energy"],
+                fits.getheader(f, 0)["Sample Theta"],
+                fits.getheader(f, 0)["Beam Current"],
+            ]
+            for f in file_list
+        ]
+        self.images = np.squeeze(np.array([[fits.getdata(f, 2) for f in file_list]]))
+        (
+            self.energies,
+            self.sample_theta,
+            self.beam_current,
+        ) = np.column_stack(arrays)
 
-        for i, file in enumerate(file_list):
-            with fits.open(file.path) as hdul:
-                header = hdul[0].header
-                energy = header["Beamline Energy"]
-                sample_theta = header["Sample Theta"]
-                beam_current = header["Beam Current"]
-                image_data = hdul[2].data
-
-                # Store data in the pre-allocated numpy arrays
-                self.energies[i] = energy
-                self.sample_theta[i] = sample_theta
-                self.beam_current[i] = beam_current
-                self.images[i] = image_data
-
+        # Calculate the scattering vector
         self.q = scattering_vector(self.energies, self.sample_theta)
-        if stitch_color == "yes":
-            self.currents = 1
-        else:
-            self.currents = self.beam_current.mean()
 
-    def reduce(self, error_method="shot") -> None:
+    def _data_reduction(self, error_method="shot") -> None:
         bright_spots, dark_spots = zip(*[locate_spot(image) for image in self.images])
         bright_spots = np.stack(bright_spots)
         dark_spots = np.stack(dark_spots)
@@ -64,33 +78,68 @@ class XRR:
 
         dark_std = np.array([np.std(np.ravel(u)) for u in dark_spots])
 
-        intensity = bright_sum - dark_sum
-
+        r = (bright_sum - dark_sum) / self.beam_current
         self.std_err = dark_std / self.q.size
         self.shot_err = np.sqrt(bright_sum + dark_sum)
-        if error_method == "shot" or error_method == None:
-            self.q, self.r = normalize(self.q, unumpy.uarray(intensity, self.shot_err))
-        if error_method == "std":
-            self.q, self.r = normalize(self.q, unumpy.uarray(intensity, self.std_err))
+        if error_method == "shot":
+            self.r = unumpy.uarray(r, self.shot_err)
+        elif error_method == "std":
+            self.r = unumpy.uarray(r, self.std_err)
+        elif error_method == "compare":
+            self.r1 = unumpy.uarray(r, self.shot_err)
+            self.r2 = unumpy.uarray(r, self.std_err)
+        else:
+            raise Exception(
+                'Choose "shot" for possonian statistics, "std" for gaussian statistics, or "compare" to compare the two methods'
+            )
 
-    def stitch(self):
-        self.q, self.r = stitch_arrays(self.q, self.r)
+    def _normalize(self):
+        """
+        Normalization
+        """
+        # find the cutoff for i_zero points
+        izero_count = np.count_nonzero(self.q == 0)
+
+        izero = uaverage(self.r[: izero_count - 1]) if izero_count > 0 else ufloat(1, 0)
+
+        self.r = self.r[izero_count:] / izero
+        self.q = self.q[izero_count:]
+
+    def _stitch(self, tol=0.1):
+        # split self.q and self.r into sections that are monotonically increasing
+        ind = np.where(np.diff(self.q) < 0)[0] + 1
+
+        q_split = np.split(self.q, ind)
+        r_split = np.split(self.r, ind)
+        self.q_split = [sub for sub in q_split if len(sub) > 3]
+        self.r_split = [sub for sub in r_split if len(sub) > 3]
+
+        for i, (sub_q, sub_r) in enumerate(zip(self.q_split, self.r_split)):
+            if i == len(self.q_split) - 1:
+                pass
+            else:
+                ratio = np.array([])  # keep track of where the sections overlap
+                for j, q in enumerate(sub_q):
+                    stitch_indices = np.where(
+                        np.isclose(self.q_split[i + 1], q, rtol=tol)
+                    )[0]
+                    next_r_values = self.r_split[i + 1][stitch_indices]
+                    r_value = sub_r[j]
+                    ratio = np.append(ratio, r_value / next_r_values)
+                ratio = uaverage(ratio)
+                self.r_split[i + 1] = self.r_split[i + 1] * ratio
 
     def plot(self) -> None:
         """Plot the X-ray reflectometry data and a reference curve."""
-
-        R = unumpy.nominal_values(self.r)
-        R_err = unumpy.std_devs(self.r)
-
-        thommas = np.loadtxt(
-            f"{os.getcwd()}\\tests\\TestData\\test.csv ",
-            skiprows=1,
-            delimiter=",",
-            usecols=(1, 2, 3),
-        )
-
-        plt.errorbar(self.q, R, R_err)
-        plt.errorbar(thommas[:, 0], thommas[:, 1], yerr=thommas[:, 2])
+        for i, sub in enumerate(zip(self.q_split, self.r_split)):
+            plt.errorbar(
+                sub[0],
+                unumpy.nominal_values(sub[1]),
+                unumpy.std_devs(sub[1]),
+                fmt=".",
+                label=f"Scan = {i+2}",
+            )
+        plt.legend()
         plt.yscale("log")
         plt.show()
 
@@ -127,76 +176,8 @@ def locate_spot(image: np.ndarray) -> tuple:
     return u_light, u_dark
 
 
-def normalize(Q: np.ndarray, R: np.ndarray) -> tuple:
-    """
-    Normalization
-    """
-    # find the cutoff for i_zero points
-    izero_count = np.count_nonzero(Q == 0)
-
-    izero = uaverage(R[: izero_count - 1]) if izero_count else ufloat(1, 0)
-
-    R = R[izero_count:] / izero
-    Q = Q[izero_count:]
-
-    return Q, R
-
-
-def stitch_arrays(Q: np.ndarray, R: np.ndarray, tol: float = 1e-6) -> tuple:
-    """
-    Stitch reflectivity curve
-    """
-    split_points = np.where(np.diff(Q) < 0)[0] + 1
-
-    subsets_Q = np.split(Q, split_points)
-    subsets_R = np.split(R, split_points)
-
-    q_sub = [sub for sub in subsets_Q if len(sub) > 3]
-    r_sub = [sub for sub in subsets_R if len(sub) > 3]
-
-    for i in range(0, np.shape(q_sub)[0] - 1):
-        curr_q_sub = q_sub[i]
-        curr_r_sub = r_sub[i]
-        next_r_sub = r_sub[i + 1]
-        next_q_sub = q_sub[i + 1]
-        ratio = []
-
-        # Create a boolean mask indicating where elements of curr_q_sub appear in next_q_sub within the given tolerance
-        mask = np.zeros((len(next_q_sub), len(curr_q_sub)), dtype=bool)
-        for j in range(len(next_q_sub)):
-            for k in range(len(curr_q_sub)):
-                mask[j, k] = np.isclose(
-                    next_q_sub[j], curr_q_sub[k], rtol=tol, atol=tol
-                )
-
-        # Calculate ratios using broadcasting
-        ratios = np.minimum.outer(curr_r_sub, next_r_sub[mask]) / np.maximum.outer(
-            curr_r_sub, next_r_sub[mask]
-        )
-
-        # Add ratios to list and flatten
-        ratio.append(ratios.flatten())
-
-        # Scale next_r_sub by mean of ratios along axis 0
-        scale_factor = uaverage(np.array(ratio), axis=0)
-        r_sub[i + 1] = next_r_sub * scale_factor
-
-    return np.concatenate(q_sub), np.concatenate(r_sub)
-
-
-def test_stitchs():
-    Rs = refl.r
-    Qs = refl.q
-    for i, sub in enumerate(Qs):
-        plt.plot(sub, unumpy.nominal_values(Rs[i]))
-    plt.yscale("log")
-    plt.show()
-
-
 if __name__ == "__main__":
     dir = f"{os.getcwd()}\\tests\\TestData\\Sorted\\282.5"
-    refl = XRR(dir)
-    refl.load()
-    refl.reduce()
-    refl.stitch()
-    print(refl.r)
+    xrr = XRR(dir)
+    xrr.calc_xrr()
+    xrr.plot()

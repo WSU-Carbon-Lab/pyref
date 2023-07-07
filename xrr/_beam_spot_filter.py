02051a488b4554c990cc18c8de3f6c731fb469de
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2023-07-06 18:17:25 -0700

    General Things

---
+++ xrr/_beam_spot_filter.py
@@ -0,0 +1,12 @@
+# Basic Filter: Define the distance between two indexes as the L1 norm. This places the a max
+#               on this distance of 4 units by default
+#
+#
+#
+#
+
+class BeamSpotFinder:
+
+    @staticmethod
+    def basicFilter():
+        

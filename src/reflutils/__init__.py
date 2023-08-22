6e358eb007a21a44a5ea6270cd1a2b20e15ee0ba
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2023-08-22 12:07:38 -0700

    fixed imports

--- src/reflutils/__init__.py
+++ src/reflutils/__init__.py
@@ -4,13 +4,13 @@ __author__ = """Harlan Heilman"""
 __email__ = "Harlan.Heilman@wsu.edu"
 __version__ = "0.1.0"
 
-from reflutils._config import *
-from reflutils.beam_spot_filter import *
-from reflutils.data_sorter import *
-from reflutils.image_manager import *
-from reflutils.load_fits import *
-from reflutils.refl_manager import *
-from reflutils.refl_reuse import *
-from reflutils.toolkit import *
-from reflutils.xrr import *
-from reflutils.display_path import *
+from . _config import *
+from . beam_spot_filter import *
+from . data_sorter import *
+from . image_manager import *
+from . load_fits import *
+from . refl_manager import *
+from . refl_reuse import *
+from . toolkit import *
+from . xrr import *
+from . display_path import *

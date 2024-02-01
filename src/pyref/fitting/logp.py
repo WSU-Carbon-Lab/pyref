72dfec022cd940a81a75b8f491424b24187901f4
HarlanHeilman <Harlan.Heilman@wsu.edu>
2024-02-01 10:08:14 -0800

    some fits

--- src/pyref/fitting/logp.py
+++ src/pyref/fitting/logp.py
@@ -15,6 +15,8 @@ class LogpExtra(object):
             rough_pars = sort_pars(pars.flattened(), "rough")
             bire_pars = sort_pars(pars.flattened(), "bire")
             delta_val = sort_pars(pars.flattened(), "dt", vary=True)
+            zz_pars = sort_pars(pars.flattened(), "zz")
+            xx_pars = sort_pars(pars.flattened(), "xx")
             ##Check that the roughness is not out of control
             for i in range(len(rough_pars)):  ##Sort through the # of layers
                 if (
@@ -39,6 +41,14 @@ class LogpExtra(object):
                     if (
                         float(bire - 3 * delta_val[0]) < 0
                         or float(bire + 3 * delta_val[0] / 2) > 0
+                    ):
+                        return -np.inf
+            # check if xx is less than delta_val or zz is greater than delta_val
+            for i in range(len(zz_pars)):
+                if zz_pars[i].vary or xx_pars[i].vary:
+                    if (
+                        zz_pars[i].value > delta_val[0]
+                        or xx_pars[i].value < delta_val[0]
                     ):
                         return -np.inf
 
@@ -74,6 +84,40 @@ class LogpExtra_rough(object):
 
     ##Function to sort through ALL parameters in an objective and return based on name keyword
     ##Returns a list of parameters for further use
+
+
+class LogpExtra_delta(object):
+    def __init__(self, objective):
+        # we'll store the parameters and objective in this object
+        # this will be necessary for pickling in the future
+        self.objective = objective  ##Full list of parameters
+
+    def __call__(self, model, data):
+        ##First constraint condition ---
+        ##Load Parameters of interest for each objective
+        for pars in self.objective.parameters:
+            delta_val = sort_pars(pars.flattened(), "delta", vary=True)
+            thick_pars = sort_pars(pars.flattened(), "thick")
+            rough_pars = sort_pars(pars.flattened(), "rough")
+            ##Check that the roughness is not out of control
+            for i in range(len(rough_pars)):  ##Sort through the # of layers
+                if (
+                    rough_pars[i].vary or thick_pars[i].vary
+                ):  # Only constrain parameters that vary
+                    interface_limit = (
+                        np.sqrt(2 * np.pi) * rough_pars[i].value / 2
+                    )  # (rough_pars[i].value/(np.sqrt(2*np.pi)))**2
+                    if (
+                        float(thick_pars[i].value - interface_limit) < 0
+                    ):  # If the interface width is above the corresponding thickness, set logp to -inf
+                        return -np.inf
+
+            # Check that the delta value is not out of control
+            if delta_val[0].vary:
+                if delta_val[0].value < 0:
+                    return -np.inf
+
+        return 0  ##If all the layers are within the constraint return 0
 
 
 def sort_pars(pars, str_check, vary=None):

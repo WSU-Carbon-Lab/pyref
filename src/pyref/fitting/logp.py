a8a6dbbf381cc2ebb62a946ce8393038d0d8d2d3
HarlanHeilman <Harlan.Heilman@wsu.edu>
2024-02-14 20:36:58 -0800

    poetry modification

--- src/pyref/fitting/logp.py
+++ src/pyref/fitting/logp.py
@@ -1,132 +1,114 @@
-import numpy as np
+from typing import Literal
+
+from numpy import inf, pi, sqrt
+
+type Parameter = Literal["thick_rough", "birefringence", "delta"]
 
 
 class LogpExtra(object):
-    def __init__(self, objective):
+    def __init__(
+        self,
+        objective,
+        surface_label: str = "surf",
+        constraints: list[Parameter] = ["thick_rough", "birefringence", "delta"],
+    ):
         # we'll store the parameters and objective in this object
         # this will be necessary for pickling in the future
         self.objective = objective  ##Full list of parameters
-
-    def __call__(self, model, data):
-        ##First constraint condition ---
-        ##Load Parameters of interest for each objective
-        for pars in self.objective.parameters:
-            thick_pars = sort_pars(pars.flattened(), "thick")
-            rough_pars = sort_pars(pars.flattened(), "rough")
-            bire_pars = sort_pars(pars.flattened(), "bire")
-            delta_val = sort_pars(pars.flattened(), "dt", vary=True)
-            zz_pars = sort_pars(pars.flattened(), "zz")
-            xx_pars = sort_pars(pars.flattened(), "xx")
-            ##Check that the roughness is not out of control
-            for i in range(len(rough_pars)):  ##Sort through the # of layers
-                if (
-                    rough_pars[i].vary or thick_pars[i].vary
-                ):  # Only constrain parameters that vary
-                    interface_limit = (
-                        np.sqrt(2 * np.pi) * rough_pars[i].value / 2
-                    )  # (rough_pars[i].value/(np.sqrt(2*np.pi)))**2
-                    if (
-                        float(thick_pars[i].value - interface_limit) < 0
-                    ):  # If the interface width is above the corresponding thickness, set logp to -inf
-                        return -np.inf
-            # Check to see if the Birefringence is physical based on current trace
-            for bire in bire_pars:
-                if bire.vary and delta_val[0] > 0:
-                    if (
-                        float(bire - 3 * delta_val[0]) > 0
-                        or float(bire + 3 * delta_val[0] / 2) < 0
-                    ):
-                        return -np.inf
-                if bire.vary and delta_val[0] < 0:
-                    if (
-                        float(bire - 3 * delta_val[0]) < 0
-                        or float(bire + 3 * delta_val[0] / 2) > 0
-                    ):
-                        return -np.inf
-            # check if xx is less than delta_val or zz is greater than delta_val
-            for i in range(len(zz_pars)):
-                if zz_pars[i].vary or xx_pars[i].vary:
-                    if (
-                        zz_pars[i].value > delta_val[0]
-                        or xx_pars[i].value < delta_val[0]
-                    ):
-                        return -np.inf
-
-        return 0  ##If all the layers are within the constraint return 0
-
-
-class LogpExtra_rough(object):
-    def __init__(self, objective):
-        # we'll store the parameters and objective in this object
-        # this will be necessary for pickling in the future
-        self.objective = objective  ##Full list of parameters
-
-    def __call__(self, model, data):
-        ##First constraint condition ---
-        ##Load Parameters of interest for each objective
-        for pars in self.objective.parameters:
-            thick_pars = sort_pars(pars.flattened(), "thick")
-            rough_pars = sort_pars(pars.flattened(), "rough")
-            ##Check that the roughness is not out of control
-            for i in range(len(rough_pars)):  ##Sort through the # of layers
-                if (
-                    rough_pars[i].vary or thick_pars[i].vary
-                ):  # Only constrain parameters that vary
-                    interface_limit = (
-                        np.sqrt(2 * np.pi) * rough_pars[i].value / 2
-                    )  # (rough_pars[i].value/(np.sqrt(2*np.pi)))**2
-                    if (
-                        float(thick_pars[i].value - interface_limit) < 0
-                    ):  # If the interface width is above the corresponding thickness, set logp to -inf
-                        return -np.inf
-
-        return 0  ##If all the layers are within the constraint return 0
-
-    ##Function to sort through ALL parameters in an objective and return based on name keyword
-    ##Returns a list of parameters for further use
-
-
-class LogpExtra_rough(object):
-    def __init__(self, objective):
-        # we'll store the parameters and objective in this object
-        # this will be necessary for pickling in the future
-        self.objective = objective  ##Full list of parameters
-
-    def __call__(self, model, data):
-        ##First constraint condition ---
-        ##Load Parameters of interest for each objective
-        pars = self.objective.parameters.flattened()
-        params_to_check = [
+        self.pars = self.objective.parameters.flattened()
+        self.terms = [
+            # value, not
             ("thick", " "),
             ("rough", " "),
-            ("_zz", "surf"),
-            ("diso", "surf"),
+            ("bire", " "),
+            ("diso", " "),
+            ("_zz", str(surface_label)),
         ]
-        sorted_pars = {
-            param: sort_pars(pars, param, not_check=notin)
-            for (param, notin) in params_to_check
+        self.constraints = constraints
+
+    def __call__(self, model, data):
+        ##First constraint condition ---
+        ##Load Parameters of interest for each objective
+
+        self.sorted_pars = {
+            par: sort_pars(self.pars, par, str_not=not_in)
+            for (par, not_in) in self.terms
         }
-        thick_pars = sorted_pars["thick"]
-        rough_pars = sorted_pars["rough"]
-        ##Check that the roughness is not out of control
+        if "thick_rough" in self.constraints:
+            self.thick_rough_constraint()
+        if "birefringence" in self.constraints:
+            self.birefringence_constraint()
+        if "delta" in self.constraints:
+            self.delta_constraint()
+
+        return 0
+
+    def thick_rough_constraint(self):
+        thick_pars = self.sorted_pars["thick"]
+        rough_pars = self.sorted_pars["rough"]
         for i in range(len(rough_pars)):  ##Sort through the # of layers
             if (
                 rough_pars[i].vary or thick_pars[i].vary
             ):  # Only constrain parameters that vary
                 interface_limit = (
-                    np.sqrt(2 * np.pi) * rough_pars[i].value / 2
+                    sqrt(2 * pi) * rough_pars[i].value / 2
                 )  # (rough_pars[i].value/(np.sqrt(2*np.pi)))**2
                 if (
                     float(thick_pars[i].value - interface_limit) < 0
                 ):  # If the interface width is above the corresponding thickness, set logp to -inf
-                    return -np.inf
+                    return -inf
 
-        delta_zz = sorted_pars["_zz"]
-        diso = sorted_pars["diso"]
-        for dz, di in zip(delta_zz, diso):
-            if dz.vary or di.vary:
-                if dz.value > di.value:
-                    return -np.inf
+    def birefringence_constraint(self):
+        bire_pars = self.sorted_pars["bire"]
+        delta_val = self.sorted_pars["diso"]
+        for bire in bire_pars:
+            if bire.vary and delta_val[0] > 0:
+                if (
+                    float(bire - 3 * delta_val[0]) > 0
+                    or float(bire + 3 * delta_val[0] / 2) < 0
+                ):
+                    return -inf
+            if bire.vary and delta_val[0] < 0:
+                if (
+                    float(bire - 3 * delta_val[0]) < 0
+                    or float(bire + 3 * delta_val[0] / 2) > 0
+                ):
+                    return -inf
+
+    def delta_constraint(self):
+        delta_val = self.sorted_pars["diso"]
+        zz_val = self.sorted_pars["_zz"]
+        # Remove the surface values
+        for i in range(len(delta_val)):
+            if delta_val[i].vary and "surf" not in delta_val[i].name:
+                if delta_val[i].value < zz_val.value:
+                    return -inf
+
+
+class LogpExtra_rough(object):
+    def __init__(self, objective):
+        # we'll store the parameters and objective in this object
+        # this will be necessary for pickling in the future
+        self.objective = objective  ##Full list of parameters
+
+    def __call__(self, model, data):
+        ##First constraint condition ---
+        ##Load Parameters of interest for each objective
+        for pars in self.objective.parameters:
+            thick_pars = sort_pars(pars.flattened(), "thick")
+            rough_pars = sort_pars(pars.flattened(), "rough")
+            ##Check that the roughness is not out of control
+            for i in range(len(rough_pars)):  ##Sort through the # of layers
+                if (
+                    rough_pars[i].vary or thick_pars[i].vary
+                ):  # Only constrain parameters that vary
+                    interface_limit = (
+                        sqrt(2 * pi) * rough_pars[i].value / 2
+                    )  # (rough_pars[i].value/(np.sqrt(2*np.pi)))**2
+                    if (
+                        float(thick_pars[i].value - interface_limit) < 0
+                    ):  # If the interface width is above the corresponding thickness, set logp to -inf
+                        return -inf
 
         return 0  ##If all the layers are within the constraint return 0
 
@@ -134,11 +116,11 @@ class LogpExtra_rough(object):
     ##Returns a list of parameters for further use
 
 
-def sort_pars(pars, str_check, vary=None, not_check=" "):
+def sort_pars(pars, str_check, vary=None, str_not=" "):
     temp = []
     num = len(pars)
     for i in range(num):
-        if str_check in pars[i].name and not_check not in pars[i].name:
+        if str_check in pars[i].name and str_not not in pars[i].name:
             if vary == True:
                 if pars[i].vary == True:
                     temp.append(pars[i])

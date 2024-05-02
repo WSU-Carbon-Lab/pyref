4a1537db715cee4c4b35013238b9d8a9306b893a
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2024-05-01 19:54:39 -0700

    Merge branch 'pre-release' of https://github.com/WSU-Carbon-Lab/pyref into pre-release

--- src/core_rust.rs
+++
@@ -1,9 +0,0 @@
-/*
-Core Rust functions for indexing the fits files from a data directory and loading their
-contents in a dataframe.
-*/
-
-use polars::prelude::*;
-use dirs;
-use astrors::io;
-use std::fs::File;

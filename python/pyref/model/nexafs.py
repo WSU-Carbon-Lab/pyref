ee8242933a78b11cbd1e4dba6681a8942830fc85
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2024-05-06 01:49:38 -0700

    Create new data class for NEXAFS data

--- python/pyref/model/nexafs.py
+++ python/pyref/model/nexafs.py
@@ -11,3 +11,21 @@ from periodictable import formula
 # ---------------------------------------------------------------------
 # Functions to read NEXAFS data from a file
 # ---------------------------------------------------------------------
+
+
+@dataclass
+class Nexafs:
+    """Class to store NEXAFS data."""
+
+    file: Path | str
+    data: pl.DataFrame | None = None
+    angle: float = 55.4
+
+    def __post_init__(self):
+        if isinstance(self.file, str):
+            self.file = Path(self.file)
+        if not self.file.is_dir():
+            self.data = pl.read_csv(self.file)
+        else:
+            msg = f"Cannot read data from directory: {self.file}"
+            raise ValueError(msg)

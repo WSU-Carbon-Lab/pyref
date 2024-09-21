33add6d6e4e347b872c76d8468d6eba7fe0b154d
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2024-09-20 17:40:56 -0700

    Selecting a better edge case

--- python/pyref/test.py
+++ python/pyref/test.py
@@ -4,9 +4,7 @@ import polars as pl
 import pyref_rs as rs
 
 df = pl.DataFrame(
-    rs.py_read_fits(
-        "C:/Users/hduva/.projects/pyref/pyref/test/ZnPc_20nm_A85042-00001.fits"
-    )
+    rs.py_read_fits("C:/Users/hduva/.projects/pyref/test/ZnPc_20nm_A85042-00001.fits")
 )
 
 print(df)

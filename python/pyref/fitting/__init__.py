828438c8849a97c761b443c757642c2920aab49e
Harlan <Harlan.Heilman@wsu.edu>
2025-02-13 11:58:50 -0800

    fix(fitting): add NexafsSLD to __all__

--- python/pyref/fitting/__init__.py
+++ python/pyref/fitting/__init__.py
@@ -7,7 +7,7 @@ from refnx.dataset import ReflectDataset
 
 from pyref.fitting.logp import LogpExtra
 from pyref.fitting.reflectivity import PXR_ReflectModel
-from pyref.fitting.structure import PXR_SLD, PXR_MaterialSLD
+from pyref.fitting.structure import PXR_SLD, PXR_MaterialSLD, PXR_NexafsSLD
 
 demove = [(DEMove(sigma=1e-7), 0.95), (DEMove(sigma=1e-7), 0.05)]
 gmove = GaussianMove(1e-7)
@@ -21,6 +21,7 @@ __all__ = [
     "LogpExtra",
     "Objective",
     "PXR_MaterialSLD",
+    "PXR_NexafsSLD",
     "PXR_ReflectModel",
     "ReflectDataset",
     "Transform",

033a78ad86c5bdb696ed8aa6060ff1921b2c496f
Harlan <Harlan.Heilman@wsu.edu>
2025-02-13 14:22:18 -0800

    fix(fitting): added df - refnx.ReflectDataset converter

--- python/pyref/fitting/refnx_converters.py
+++ python/pyref/fitting/refnx_converters.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import numpy as np
 from refnx.dataset import ReflectDataset
 
 if TYPE_CHECKING:
@@ -12,14 +11,32 @@ if TYPE_CHECKING:
 
 
 def to_reflect_dataset(
-    df: pl.DataFrame, *, overwrite_err: bool = True
-) -> ReflectDataset:
+    df: pl.DataFrame, *, gb_energy="Beamline Energy [eV]", overwrite_err: bool = True
+) -> ReflectDataset | list[ReflectDataset]:
     """Convert a pandas dataframe to a ReflectDataset object."""
     if not overwrite_err:
         e = "overwrite_err=False is not implemented yet."
         raise NotImplementedError(e)
-    Q = df["Q"].to_numpy()
-    R = df["r"].to_numpy()
-    dR = np.sqrt(np.square(R) + np.square(Q))
+    datasets = []
+    for _, g in df.group_by(gb_energy):
+        Q = g["Q"].to_numpy()
+        R = g["r"].to_numpy()
+        dR = 0.15 * R + 0.5e-6 * Q
+        ds = ReflectDataset(data=(Q, R, dR))
+        datasets.append(ds)
+    if len(datasets) == 1:
+        return datasets[0]
+    return datasets
 
-    return ReflectDataset((Q, R, dR))
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import polars as pl
+
+    df = pl.read_parquet("/home/hduva/projects/data/june_processed.parquet")
+    ds = to_reflect_dataset(df.filter(pl.col("sample").str.starts_with("mono")))
+
+    for s in ds:
+        s.plot()
+        plt.yscale("log")
+        plt.show()

967980cafe144b9ea4cd44d2835e09ea55f13a83
Harlan <Harlan.Heilman@wsu.edu>
2025-02-13 17:20:04 -0800

    fix: typo

--- python/pyref/fitting/structure.py
+++ python/pyref/fitting/structure.py
@@ -1200,8 +1200,8 @@ class NexafsSLD(Scatterer):
                 # Set bounds
                 self.birefringence.bounds = (
                     (5 * self._initial_birefr, 0)
-                    if self._initial_bireff < 0
-                    else (0, 5 * self._initial_bireff)
+                    if self._initial_birefr < 0
+                    else (0, 5 * self._initial_birefr)
                 )
                 # ensure density and rotation are fixed to reduce parameter space
                 self.density.setp(value=1, vary=False)
